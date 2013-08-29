c6d1a8c0941e5a23fc13fdf76668deedf3b57a0b
straup <straup@gmail.com>
2013-08-29 08:57:30 -0400

    logging; query params; load boilerpipe but still doesn't work

--- src/main/java/info/aaronland/extruder/ExtruderResource.java
+++ src/main/java/info/aaronland/extruder/ExtruderResource.java
@@ -1,22 +1,44 @@
 package info.aaronland.extruder;
 
-// import info.aaronland.extruder.ExtruderConfiguration;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import java.net.URL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.l3s.boilerpipe.BoilerpipeExtractor;
+import de.l3s.boilerpipe.extractors.CommonExtractors;
+import de.l3s.boilerpipe.sax.HTMLHighlighter;
 
 @Path(value = "/extrude")
 @Produces(MediaType.TEXT_PLAIN)
 public class ExtruderResource {
 
-    public ExtruderResource(){
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExtruderResource.class);
 
-    }
+    /*
+    final BoilerpipeExtractor extractor = CommonExtractors.ARTICLE_EXTRACTOR;
+    
+    final boolean includeImages = true;
+    final boolean bodyOnly = false;
+    final HTMLHighlighter hh = HTMLHighlighter.newExtractingInstance(includeImages, bodyOnly);
+    */
 
     @GET
-    public String extrudeThis(){
+    public String extrudeThis(@QueryParam("id") String id){
+
+	LOGGER.info("GET ME " + id);
+
+	/*
+	URL url = new URL("");
+	String extractedHtml = hh.process(url, extractor);
+	*/
+
 	return new String("HELLO");
     }
 
