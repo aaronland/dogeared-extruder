e181adafbf987ec6db7d6e08d3610e19108b0b81
straup <straup@gmail.com>
2013-08-31 10:34:31 -0400

    make tika parse a document... that was easy

--- src/main/java/info/aaronland/extruder/TikaResource.java
+++ src/main/java/info/aaronland/extruder/TikaResource.java
@@ -10,9 +10,20 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.MediaType;
 
 import java.net.URL;
+import java.net.URLConnection;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.BufferedInputStream;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.ContentHandler;
 
 @Path(value = "/tika")
 @Produces(MediaType.TEXT_PLAIN)
@@ -37,17 +48,32 @@ public class TikaResource {
             return Response.status(Response.Status.NOT_ACCEPTABLE).build();
 	}
 
-	
-	/*
-	try {
+	BufferedInputStream buffer = null;
 
+	try {
+	    URLConnection conn = url.openConnection();
+	    buffer = new BufferedInputStream(conn.getInputStream());
 	}
 
 	catch (Exception e){
 	    LOGGER.error(e.toString());
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 	}
-	*/
+	
+	try {
+
+	    Parser parser = new AutoDetectParser();
+	    ContentHandler handler = new BodyContentHandler(System.out);
+	    
+	    Metadata metadata = new Metadata();
+ 
+	    parser.parse(buffer, handler, metadata, new ParseContext());
+	}
+
+	catch (Exception e){
+	    LOGGER.error(e.toString());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+	}
 
 	return Response.status(Response.Status.OK).entity(text).build();
     }
