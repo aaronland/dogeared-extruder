a8499347ed4ff9520fed9c62da1d60dbb9c49d52
straup <straup@gmail.com>
2013-09-02 08:58:48 -0400

    try adding views because... java?

---
+++ src/main/java/info/aaronland/extruder/DocumentView.java
@@ -0,0 +1,17 @@
+package info.aaronland.extruder;
+
+import info.aaronland.extruder.Document;
+import com.yammer.dropwizard.views.View;
+
+public class DocumentView extends View {
+    private final Document document;
+
+    public DocumentView(Document document) {
+        super("document.ftl");
+        this.document = document;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+}
