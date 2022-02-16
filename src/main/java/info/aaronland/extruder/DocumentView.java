13d88d9f2e97708b60370c101e314ccdc82f1c5f
thisisaaronland <aaron@localhost>
2022-02-16 14:52:24 -0800

    snapshot: load freemarker stuff; new HTML errors

--- src/main/java/info/aaronland/extruder/DocumentView.java
+++ src/main/java/info/aaronland/extruder/DocumentView.java
@@ -3,10 +3,6 @@ package info.aaronland.extruder;
 import info.aaronland.extruder.Document;
 import io.dropwizard.views.View;
 
-//import com.google.common.base.Charsets;
-//import com.google.common.base.Optional;
-//import java.nio.charset.Charset;
-
 public class DocumentView extends View {
     private final Document document;
 
@@ -18,14 +14,5 @@ public class DocumentView extends View {
     public Document getDocument(){
         return document;
     }
-
-    // Because in com/codahale/dropwizard/views/freemarker/FreemarkerViewRenderer.java this:
-    // final Charset charset = view.getCharset().or(Charset.forName(configuration.getEncoding(locale)));
-    // And since the default encoding for en-us is ISO-8859-1... good times
-    // (20130908/straup)
-
-    //public Optional<Charset> getCharset(){
-    //return Optional.of(Charsets.UTF_8);
-    // }
 
 }
